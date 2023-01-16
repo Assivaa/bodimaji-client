91e2ad80c0001bc6a90bc4a31112fba9bbea8ea1
rreyvido <reyvidod@outlook.com>
2023-01-16 20:59:31 +0700

    article page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -58,32 +58,83 @@ export const ImageCard = ({ product }) => {
   );
 };
 
-export const ArticleCard = () => {
+export const ArticleCard = ({ article }) => {
   return (
-    <div className="flex justify-center">
-      <div className="rounded-lg shadow-lg bg-white max-w-sm">
-        <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <img
-            className="rounded-t-lg"
-            src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
-            alt=""
-          />
-        </a>
-        <div className="p-6">
-          <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-          <p className="text-gray-700 text-base mb-4">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button
-            type="button"
-            className="inline-block px-7 py-3 mr-2 bg-black text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red-900 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Read More
-          </button>
+    <div className="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow">
+      <Link to={`/article/${article._id}`}>
+        <img
+          src="https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+          className="aspect-video w-full object-cover"
+          alt=""
+        />
+      </Link>
+      <div className="p-4">
+        <p className="mb-1 text-sm text-primary-500">
+          {article.author} • <time>18 Nov 2022</time>
+        </p>
+        <h3 className="text-xl font-medium text-gray-900">{article.title}</h3>
+        <p className="mt-1 text-gray-500">{article.description}</p>
+        <div className="mt-4 flex gap-2">
+          <span className="inline-flex items-center gap-1 rounded-full bg-red-50 px-2 py-1 text-xs font-semibold text-red-900">
+            Design
+          </span>
+          <span className="inline-flex items-center gap-1 rounded-full bg-indigo-50 px-2 py-1 text-xs font-semibold text-indigo-600">
+            Product
+          </span>
+          <span className="inline-flex items-center gap-1 rounded-full bg-orange-50 px-2 py-1 text-xs font-semibold text-orange-600">
+            Develop
+          </span>
         </div>
       </div>
     </div>
+  );
+};
+
+export const SingleArticleCard = ({ article }) => {
+  let dateFormat = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let createdDate = new Date(article.createdAt);
+  const createdTime = createdDate.toLocaleDateString("en-US", dateFormat);
+  return (
+    <>
+      <main class="mt-full">
+        <div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative">
+          <div class="left-0 bottom-0 w-full h-full z-10"></div>
+          <img
+            src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+            class="left-0 top-0 w-full h-full z-0 object-cover"
+          />
+          <div class="p-4 bottom-0 left-0 z-20">
+            <a
+              href="#"
+              class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2"
+            >
+              Nutrition
+            </a>
+            <h2 class="text-4xl font-semibold text-black leading-tight">
+              {article.title}
+            </h2>
+            <div class="flex mt-3">
+              <div>
+                <p class="font-semibold text-black text-sm">{article.author}</p>
+                <p class="font-semibold text-black text-xs"> {createdTime}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+          <p class="pb-6">{article.description}</p>
+        </div>
+      </main>
+      <h2 class="text-2xl mt-4 text-black font-bold text-center">
+        Related Article
+      </h2>
+    </>
   );
 };
 
