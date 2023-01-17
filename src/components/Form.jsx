fa25acd6ae6ed54fea0181f33bcfe38f4a1a9210
rreyvido <reyvidod@outlook.com>
2023-01-17 21:27:35 +0700

    implement wishlist page, fix cart redux on login

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,6 +28,21 @@ export const LoginForm = () => {
           navigate("/");
         } else if (response.data.role === "customer") {
           dispatch(loginSuccess(response));
+          const getCart = async () => {
+            const { data } = await axios.get(
+              process.env.REACT_APP_API_URL + `/cart/${response.data._id}`
+            );
+            let arrQty = data.products;
+            if (data) {
+              if (arrQty) {
+                const totalQty = arrQty.reduce((accumulator, object) => {
+                  return accumulator + object.quantity;
+                }, 0);
+                dispatch(getQuantity(totalQty));
+              }
+            }
+          };
+          getCart();
           navigate("/");
         }
       })
@@ -250,6 +265,7 @@ export const CheckoutForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
   const currentCart = useSelector((state) => state.cart);
+  const currentProducts = useSelector((state) => state.cart.products);
 
   const nameRef = useRef(null);
   const emailRef = useRef(null);
---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { WishlistCard } from "../components/Card";
+import Footer from "../components/Footer";
+import { CircleLoading } from "../components/Loading";
+import Navbar from "../components/Navbar";
+
+export const Wishlist = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const [wishlist, setWishlist] = useState([]);
+
+  const getWishlist = async () => {
+    const { data } = await axios.get(
+      process.env.REACT_APP_API_URL + `/wishlist/${currentUser.data._id}`
+    );
+
+    if (data) {
+      setWishlist(data.products);
+    }
+  };
+
+  const results = [];
+
+  if (wishlist) {
+    wishlist.forEach((w) => {
+      results.push(
+        <WishlistCard wishlistItem={w} key={w._id} getWishlist={getWishlist} />
+      );
+    });
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    getWishlist().then((data) => {
+      setIsLoading(false);
+    });
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div className="px-6 py-20 md:px-12 bg-gray-50 text-gray-800 text-center lg:text-left">
+        <div className="container mx-auto xl:px-32">
+          <div className="grid lg:grid-cols-2 gap-12 flex-nowrap">
+            <div className="mt-12 lg:mt-0">
+              <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+                My Wishlist
+                <br />
+              </h1>
+            </div>
+          </div>
+          {isLoading ? (
+            <CircleLoading />
+          ) : (
+            <>{wishlist ? <>{results}</> : <> NO PRODUCT</>}</>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
