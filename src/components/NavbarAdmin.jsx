e28b19709be1c2e3f4a247fca822de02481bded0
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2023-01-19 22:50:44 +0700

    navbar admin

--- src/components/NavbarAdmin.jsx
+++ src/components/NavbarAdmin.jsx
@@ -4,21 +4,21 @@ const NavbarAdmin = () => {
 
     return (
         <div class="navbaradmin-wrap">
-            <div
-                class="px-8 py-6 pt-0 pb-0 flex flex-row justify-between border-b border-black align-baseline font-inter font-normal font-normal text-white bg-#221177 shadow-xl w-950 h-25 ml-80 mt-minus-40"
-                style={{ display: "flex", justifyContent: "space-between" }}
-            >
-                <div class="flex font-medium text-white text-3xl leading-normal mt-minus-30 ml-40">
-                    Bodimaji
-                </div>
-
-            </div>
             <div className="w-140 h-140 mt-minus-45 ml-40 mb-minus-55">
                 <img
                     className="w-4 mr-3"
                     alt="logogram"
                     src="https://res.cloudinary.com/dcrd7eosb/image/upload/v1673322483/shop/bodimaji-logogram_dbqmgr.png"
                 />
+            </div>
+            <div
+                class="px-8 py-6 pt-0 pb-0 flex flex-row justify-between border-b border-black align-baseline font-inter font-normal font-normal text-white bg-#221177 shadow-xl w-950 h-25 ml-80 mt-minus-40"
+                style={{ display: "flex", justifyContent: "space-between" }}
+            >
+                <div class="flex font-medium text-black text-3xl leading-normal mt-minus-30 ml-40">
+                    Bodimaji
+                </div>
+
             </div>
         </div>
     );
