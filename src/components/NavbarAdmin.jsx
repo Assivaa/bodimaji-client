d5214706d9673d8096fc639a713b543d0b1af548
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2023-01-20 19:18:39 +0700

    hapus tulisan bodimaji

--- src/components/NavbarAdmin.jsx
+++ src/components/NavbarAdmin.jsx
@@ -15,10 +15,6 @@ const NavbarAdmin = () => {
                 class="px-8 py-6 pt-0 pb-0 flex flex-row justify-between border-b border-black align-baseline font-inter font-normal font-normal text-white bg-#221177 shadow-xl w-950 h-25 ml-80 mt-minus-40"
                 style={{ display: "flex", justifyContent: "space-between" }}
             >
-                <div class="flex font-medium text-black text-3xl leading-normal mt-minus-30 ml-40">
-                    Bodimaji
-                </div>
-
             </div>
         </div>
     );
