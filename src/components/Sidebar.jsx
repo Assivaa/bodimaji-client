bf63fa47b8ac4c0cbae42d9342e11e06c09e9030
rreyvido <reyvidod@outlook.com>
2023-01-24 16:18:22 +0700

    admin sidebar logout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../redux/userSlice";
 
 const Sidebar = () => {
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    navigate("/");
+  };
   return (
     <>
       <div className="bg-gray-00">
@@ -16,7 +25,7 @@ const Sidebar = () => {
             <div className="my-2 bg-gray-600 h-[1px]"></div>
           </div>
           <Link to="/dashboard">
-            <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-red-600 text-white">
               <i className="bi bi-house-door-fill"></i>
               <span className="text-[15px] ml-4 text-gray-200 font-bold">
                 Dashboard
@@ -24,7 +33,7 @@ const Sidebar = () => {
             </div>
           </Link>
           <Link to="/dashboard/order">
-            <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-red-600 text-white">
               <i className="bi bi-bookmark-fill"></i>
               <span className="text-[15px] ml-4 text-gray-200 font-bold">
                 Order List
@@ -42,12 +51,12 @@ const Sidebar = () => {
           </div>
           <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold">
             <Link to="/dashboard/product">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 Products List
               </h1>
             </Link>
             <Link to="/dashboard/product/new">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 New Product
               </h1>
             </Link>
@@ -63,12 +72,12 @@ const Sidebar = () => {
           </div>
           <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold">
             <Link to="/dashboard/article">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 Articles List
               </h1>
             </Link>
             <Link to="/dashboard/article/new">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 New Article
               </h1>
             </Link>
@@ -84,19 +93,22 @@ const Sidebar = () => {
           </div>
           <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold">
             <Link to="/dashboard/user">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 Users List
               </h1>
             </Link>
             <Link to="/dashboard/user/new">
-              <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+              <h1 className="cursor-pointer p-2 hover:bg-red-600 rounded-md mt-1">
                 New User
               </h1>
             </Link>
           </div>
-          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-red-600 text-white">
             <i className="bi bi-box-arrow-in-right"></i>
-            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+            <span
+              className="text-[15px] ml-4 text-gray-200 font-bold"
+              onClick={handleLogout}
+            >
               Logout
             </span>
           </div>
